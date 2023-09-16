f22466350f39647c26c8782b73c2c237b76a0ffb
DanielleMauro16 <eng.danielle.mauro16@gmail.com>
2023-09-16 17:24:36 -0300

    Teste_Somar

---
+++ Projeto_teste_operacoes/operacoes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_teste_operacoes
+{
+   public static class operacoes
+    {
+        public static double Somar(double pNum, double sNum)
+        {
+            return (pNum + sNum);
+        }
+                
+    }
+}
