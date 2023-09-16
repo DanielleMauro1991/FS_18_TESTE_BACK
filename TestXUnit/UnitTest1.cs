031f113115fba9ae236babcc0203323c92e9c94e
DanielleMauro16 <eng.danielle.mauro16@gmail.com>
2023-09-16 17:42:24 -0300

    Finalizando XUnit Test

--- TestXUnit/UnitTest1.cs
+++ TestXUnit/UnitTest1.cs
@@ -1,11 +1,47 @@
+global using Xunit;
+using Projeto_teste_operacoes;
+
 namespace TestXUnit
 {
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void SomarDoisNumeros()
         {
+            //Arranje: Preparação
+
+            double pNum = 1;
+
+            double sNum = 1;
+
+            double rNum = 2;
+
+            //Act: Ação
+
+            var resultado = operacoes.Somar(pNum, sNum);
+
+            //Assert: Verificação
+
+
+            Assert.Equal(rNum, resultado);
 
         }
+        [Theory]
+        [InlineData(1, 1, 2)]
+        [InlineData(3, 3, 6)]
+        [InlineData(7, 7, 14)]
+        [InlineData(3, 2, 5)]
+
+        public void SomarDoisNumerosLista(double pNum, double sNum, double rNum)
+        {
+            //Act: Ação
+
+            var resultado = operacoes.Somar(pNum, sNum);
+
+            //Assert: Verificação
+
+            Assert.Equal(rNum, resultado);
+        }
     }
+
 }
